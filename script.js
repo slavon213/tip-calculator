5d91b6fcb9e5da22ef344173bcab9b1c31c66635
slavon213 <slavon213@gmail.com>
2025-07-11 15:01:50 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const bill = document.getElementById('people-number');
+const customTip = document.getElementById('custom-tip');
+const resetButton = document.getElementById('reset');
+console.log(resetButton);
+
+
+
+
+
