5d88a03fa397e174735261f108a985d24f647c31
slavon213 <slavon213@gmail.com>
2025-07-11 15:26:21 +0300

    begin limit input length

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const bill = document.getElementById('people-number');
-const customTip = document.getElementById('custom-tip');
-const resetButton = document.getElementById('reset');
-console.log(resetButton);
+const bill = document.getElementById("bill");
+const customTip = document.getElementById("custom-tip");
+const resetButton = document.getElementById("reset");
+console.log(bill);
 
-
-
-
-
+bill.addEventListener("input", function () {
+    this.value = this.value.replace(/[^\d.]+/g, "").substring(0, 5);
+    // console.log('test');
+    
+});
