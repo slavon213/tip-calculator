dc4de6fc7a6a962c4670c65be9f06645d8ee666b
slavon213 <slavon213@gmail.com>
2025-07-30 12:18:30 +0300

    add handler input to bill input

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const bill = document.getElementById("bill");
 const customTip = document.getElementById("custom-tip");
 const resetButton = document.getElementById("reset");
-console.log(bill);
 
 bill.addEventListener("input", function () {
-    this.value = this.value.replace(/[^\d.]+/g, "").substring(0, 5);
-    // console.log('test');
-    
+    let value = this.value;
+    value = value.replace(/[^\d.]+/g, "");
+    const parts = value.split(".");
+
+    if (parts.length > 2) {
+        value = parts[0] + "." + parts.slice(1, 3).join("");
+    }
+
+    if (value.includes(".")) {
+        const dotIndex = value.indexOf(".");
+        const intPart = value.substring(0, dotIndex);
+        let decimalPart = value.substring(dotIndex + 1);
+
+        if (decimalPart.length > 2) {
+            decimalPart = decimalPart.substring(0, 2);
+        }
+
+        value = intPart + "." + decimalPart;
+    }
+
+    this.value = value;
 });
